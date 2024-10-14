69307f6c0f4f0332cb9aa9ef26a9d7c6626a20e7
danilovukosich <vukosichdanilo@gmail.com>
2024-10-14 12:13:56 -0300

    initial commit

---
+++ app/Livewire/InstitucionIndex.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class InstitucionIndex extends Component
+{
+    public function render()
+    {
+        return view('livewire.institucion-index');
+    }
+}
